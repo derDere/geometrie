d8605f5c9048bda5049047fd4e1aa49710321fd5
Phillip Deremer <phillip@deremer.de>
2022-01-21 01:56:30 +0100

    Header definitions

--- geometrie.hpp
+++ geometrie.hpp
@@ -8,17 +8,70 @@ using namespace std;
 
 namespace geometrie {
 
-  struct Vector2D;
-  struct Rect;
+  class cPrintable;
+  class cVector2D : cPrintable;
+  class cSize : cVector2D;
+  class cPoint : cVector2D;
+  class cRect : cPrintable;
+  class cLine : cPrintable;
+  class cCircle : cPrintable;
 
-  Point operator +(Vector2D, Vector2D);
-  Point operator +(Vector2D, int);
-  Point operator -(Vector2D, Vector2D);
-  Point operator -(Vector2D, int);
-  Point operator *(Vector2D, Vector2D);
-  Point operator *(Vector2D, int);
-  Point operator /(Vector2D, Vector2D)
+  cVector2D operator +(cVector2D, cVector2D);
+  cVector2D operator +(cVector2D, float);
+  cVector2D operator +(cVector2D, int);
 
+  cVector2D operator -(cVector2D, sVector2D);
+  cVector2D operator -(cVector2D, float);
+  cvector2D operator -(cVector2D, int);
+
+  cVector2D operator *(cVector2D, sVector2D);
+  cVector2D operator *(cVector2D, float);
+  cVector2D operator *(cVector2D, int);
+
+  cVector2D operator /(cVector2D, sVector2D);
+  cVector2D operator /(cVector2D, float);
+  cVector2D operator /(cVector2D, int);
+
+  cVector2D operator %(cVector2D, sVector2D);
+  cVector2D operator %(cVector2D, float);
+  cVector2D operator %(cVector2D, int);
+
+  cVector2D operator ++(cVector2D);
+  cVector2D operator --(cVector2D);
+
+  bool operator <(cVector2D, cVector2D);
+  bool operator <(cVector2D, float);
+  bool operator <(cVector2D, int);
+
+  bool operator >(cVector2D, cVector2D);
+  bool operator >(cVector2D, float);
+  bool operator >(cVector2D, int);
+
+  bool operator <=(cVector2D, cVector2D);
+  bool operator <=(cVector2D, float);
+  bool operator <=(cVector2D, int);
+
+  bool operator >=(cVector2D, cVector2D);
+  bool operator >=(cVector2D, float);
+  bool operator >=(cVector2D, int);
+
+  bool operator ==(cVector2D, cVector2D);
+  bool operator !=(cVector2D, cVector2D);
+
+  class cPrintable {
+    public:
+      virtual string ToString();
+  }
+
+  class cVector2D {
+    public:
+      float x, y;
+
+      float GetLength();
+      void Normalize();
+
+      virtual string ToString();
+  }
 }
 
 #endif
