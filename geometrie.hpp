b08d7985cb5b7de4a0174fc675b50bb6417219c3
Phillip Deremer <deremer@orbitlog.com>
2022-01-21 18:35:52 +0100

    more header stuff

--- geometrie.hpp
+++ geometrie.hpp
@@ -9,6 +9,7 @@ using namespace std;
 namespace geometrie {
 
   class cPrintable;
+  class cAngle : cPrintable;
   class cVector2D : cPrintable;
   class cSize : cVector2D;
   class cPoint : cVector2D;
@@ -65,13 +66,115 @@ namespace geometrie {
 
   class cVector2D {
     public:
-      float x, y;
+      float a, b;
 
       float GetLength();
+      cAngle GetAngle();
       void Normalize();
+      void Rotate(float);
+      void Rotate(cAngle);
 
       virtual string ToString();
+
+      cVector2D();
+      cVector2D(int);
+      cVector2D(float);
+      cVector2D(int, int);
+      cVector2D(float, float);
+      cVector2D(int, float);
+      cVector2D(float, int);
+      cVector2D(Vector2D&);
+      cVector2D(cAngle, float);
+      cVector2D(cAngle, int);
+
+      ~cVector2D();
   }
+
+  class cAngle {
+    private:
+      float rad;
+
+    public:
+      float GetDegrees();
+      void SetDegrees(float);
+
+      float GetRadians();
+      void SetRadians(float);
+
+      void Approach(cAngle, float);
+      void Approach(cAngle, int);
+      void ApproachRadians(float, float);
+      void ApproachRadians(float, int);
+      void ApproachDegrees(int, float);
+      void ApproachDegrees(int, int);
+      void ApproachDegrees(float, float);
+      void ApproachDegrees(float, int);
+
+      cAngle();
+      cAngle(float);
+      cAngle(int);
+      cAngle(cAngle);
+      cAngle(cVector2D);
+      cAngle(cVector2D, cVector2D);
+
+      ~cAngle();
+
+      static cAngle FromDegrees(float);
+      static cAngle FromDegrees(int);
+  }
+
+  class cPoint {
+    public:
+      float GetX();
+      void SetX(float);
+      void SetX(int);
+
+      float GetY();
+      void SetY(float);
+      void SetY(int);
+  }
+
+  class cSize() {
+    public:
+      float GetWidth();
+      void SetWidth(float);
+      void SetWidth(int);
+
+      float GetHeight();
+      void SetHeight(float);
+      void SetHeight(int);
+  }
+
+  class cRect() {
+    public:
+      cPoint Position;
+      cSize Size;
+
+      float GetTop();
+      void SetTop(float);
+      void SetTop(int);
+
+      float GetLeft();
+      void SetLeft(float);
+      void SetLeft(int);
+
+      float GetRight();
+      void SetRight(float);
+      void SetRight(int);
+
+      float GetBottom();
+      void SetBottom(float);
+      void SetBottom(int);
+
+      cRect();
+      cRect(cPoint, cSize);
+      cRect(cVector2D, cVector2D);
+      cRect(float, float, float, float);
+      cRect(int, int, int, int);
+
+      ~cRect();
+  }
+
 }
 
 #endif
